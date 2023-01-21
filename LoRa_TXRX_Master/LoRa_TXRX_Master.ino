373d5f809c8461c5a82c19b2ba327cd6998bb02e
Shahil Din <dinshahil@gmail.com>
2023-01-21 23:51:13 +1300

    Added LCD

---
+++ LoRa_TXRX_Master/LoRa_TXRX_Master.ino
@@ -0,0 +1,26 @@
+
+
+
+#include "WROVER_KIT_LCD.h"
+#include "SPI.h"
+
+WROVER_KIT_LCD tft;  // create object tft
+
+
+void setup() {
+  // put your setup code here, to run once:
+   Serial.begin(115200);
+   tft.begin(); 
+   tft.setRotation(0);
+   tft.fillScreen(WROVER_BLACK);
+   tft.setCursor(0, 0);
+   tft.setTextColor(WROVER_YELLOW);  tft.setTextSize(2);
+   tft.println("LoRa Trasmitter!");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ 
+
+}
